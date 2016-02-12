1794e12ba26d75511c490a85d881f15b9f05c365
Jev Zelenkov <jevzee@icloud.com>
2016-02-12 15:07:50 -0800

    list of LocalDates between two dates

--- src/main/java/com/jevzee/daterange/DateRange.java
+++ src/main/java/com/jevzee/daterange/DateRange.java
@@ -3,6 +3,8 @@ package com.jevzee.daterange;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -24,5 +26,17 @@ public final class DateRange {
     public long periodInDays() {
         final Period between = Period.between(from, to);
         return between.get(ChronoUnit.DAYS);
+    }
+
+    public List<LocalDate> periodBetween() {
+        final List<LocalDate> result = new ArrayList<LocalDate>();
+        LocalDate rangeIterator = LocalDate.from(from);
+
+        while (rangeIterator.isBefore(to) || rangeIterator.equals(to)) {
+            result.add(LocalDate.from(rangeIterator));
+            rangeIterator = rangeIterator.plus(1, ChronoUnit.DAYS);
+        }
+
+        return result;
     }
 }
--- src/test/java/com/jevzee/daterange/DateRangeTest.java
+++ src/test/java/com/jevzee/daterange/DateRangeTest.java
@@ -6,9 +6,10 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public final class DateRangeTest {
     private DateRange oneDay;
@@ -33,6 +34,28 @@ public final class DateRangeTest {
     @Test
     public void testOneWeekPeriodInDays() throws IOException {
         assertThat(oneWeek.periodInDays(), is(7L));
+    }
+
+    @Test
+    public void testOneDayPeriodBetween() throws IOException {
+        List<LocalDate> range = oneDay.periodBetween();
+        assertThat(range, hasSize(2));
+        assertThat(range.get(0).toString(), is(LocalDate.now().minus(1, ChronoUnit.DAYS).toString()));
+        assertThat(range.get(1).toString(), is(LocalDate.now().toString()));
+    }
+
+    @Test
+    public void testOneWeekPeriodBetween() throws IOException {
+        List<LocalDate> range = oneWeek.periodBetween();
+        assertThat(range, hasSize(8));
+        assertThat(range.get(0).toString(), is(LocalDate.now().minus(7, ChronoUnit.DAYS).toString()));
+        assertThat(range.get(1).toString(), is(LocalDate.now().minus(6, ChronoUnit.DAYS).toString()));
+        assertThat(range.get(2).toString(), is(LocalDate.now().minus(5, ChronoUnit.DAYS).toString()));
+        assertThat(range.get(3).toString(), is(LocalDate.now().minus(4, ChronoUnit.DAYS).toString()));
+        assertThat(range.get(4).toString(), is(LocalDate.now().minus(3, ChronoUnit.DAYS).toString()));
+        assertThat(range.get(5).toString(), is(LocalDate.now().minus(2, ChronoUnit.DAYS).toString()));
+        assertThat(range.get(6).toString(), is(LocalDate.now().minus(1, ChronoUnit.DAYS).toString()));
+        assertThat(range.get(7).toString(), is(LocalDate.now().toString()));
     }
 
 }
